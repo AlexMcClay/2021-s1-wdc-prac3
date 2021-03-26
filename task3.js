e1a7b3a5280c22aac5e9f645fca79f11b57dcf95
AlexMcClay <64782687+AlexMcClay@users.noreply.github.com>
2021-03-26 23:06:25 +1030

    Italic and Bold done

--- task3.js
+++ task3.js
@@ -12,15 +12,6 @@ function disp() {
 // Task 3-2
 function get_time() {
 
-
-	/*var d = new Date();
-	var z = document.getElementsByClassName("post-time");
-	z[0].innerHTML = d;
-
-	var text = document.getElementById("post_text").value;
-	document.getElementsByClassName("post-content")[0].innerHTML = text;*/
-
-
 	var ammount = document.getElementById("quantity").value;
 	for (var i = 0;i < parseInt(ammount); i++) {
 		var d = document.createElement("p");
@@ -42,14 +33,24 @@ function get_time() {
 			text_disp.style.color = "Red";
 
 		}
+		if (document.getElementById('Bold').checked) {
+			text_disp.style.fontWeight = "bold";
+		}
+		if (document.getElementById('Italic').checked) {
+			text_disp.style.fontStyle = "italic";
+		}
 		z.appendChild(text_disp);
 	}
 
 
-	
-	
+	/* Old Code
 
- 
+	var d = new Date();
+	var z = document.getElementsByClassName("post-time");
+	z[0].innerHTML = d;
+
+	var text = document.getElementById("post_text").value;
+	document.getElementsByClassName("post-content")[0].innerHTML = text;*/
 }
 
 
