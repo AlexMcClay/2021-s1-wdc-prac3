1ac8925645a8db99ca19b6fe7b5c65faffd7f77a
AlexMcClay <64782687+AlexMcClay@users.noreply.github.com>
2021-03-26 20:26:18 +1030

    Fixed 3-2

--- task3.js
+++ task3.js
@@ -10,5 +10,9 @@ function get_time() {
 	var d = new Date();
 	var z = document.getElementsByClassName("post-time");
 	z[0].innerHTML = d;
+
+	var text = document.getElementById("post_text").value;
+	document.getElementsByClassName("post-content")[0].innerHTML = text;
+
  
 }
