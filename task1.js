dbd12a51cdf60f2d8e5e3f263cc2941ca2360635
AlexMcClay <64782687+AlexMcClay@users.noreply.github.com>
2021-03-26 20:09:00 +1030

    Test

--- task1.js
+++ task1.js
@@ -1,5 +1,5 @@
 function function1() {
                 var d = new Date();
-                var x = document.getElementsByTagName("p");
+                var x = document.getElementsByClassName("time_box");
                 x[1].innerText = d;
             }
--- task3.js
+++ task3.js
@@ -5,3 +5,10 @@ function disp() {
 	document.getElementById("mcount").innerHTML = x;
 
 }
+
+function get_time() {
+	var d = new Date();
+	var z = document.getElementsByClassName("post-time");
+	z[0].innerHTML = d;
+ 
+}
