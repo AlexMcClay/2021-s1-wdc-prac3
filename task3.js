f2f0301d7c182912da61be542f3a267d6c0158d1
AlexMcClay <64782687+AlexMcClay@users.noreply.github.com>
2021-03-26 22:14:34 +1030

    test -1

--- task3.js
+++ task3.js
@@ -11,38 +11,28 @@ function disp() {
 
 // Task 3-2
 function get_time() {
-	var d = new Date();
+
+
+	/*var d = new Date();
 	var z = document.getElementsByClassName("post-time");
 	z[0].innerHTML = d;
 
 	var text = document.getElementById("post_text").value;
-	document.getElementsByClassName("post-content")[0].innerHTML = text;
+	document.getElementsByClassName("post-content")[0].innerHTML = text;*/
 
 
-	/*var d = document.createElement("div");
+	var d = document.createElement("div");
 	d.innerHTML = new Date();
 	d.className = "post-time";
 	var z = document.getElementById("posts");
 	z.appendChild(d);
-
-
-	  OLD CODE
-	var date = document.createElement("div");
-	var node = document.createTextNode("This is new.")
-	date.appendChild = node;
-	var d = new Date();
-	date.innerHTML = d;
-	date.setAttributes("class") =  "post-time";
-	document.getElementById("posts").appendChild(date);
-	
-
 
 	var text_disp = document.createElement("div");
 	var text = document.getElementById("post_text").value;
 	text_disp.innerHTML = text;
 	text_disp.className = "post-content";
 	z.appendChild(text_disp);
-	*/
+	
 
  
 }
