84de040d6753d57a0474ad0310db8f1a2a1e54b2
AlexMcClay <64782687+AlexMcClay@users.noreply.github.com>
2021-03-26 22:10:25 +1030

    Task 3-4 v1

--- task3.js
+++ task3.js
@@ -1,22 +1,46 @@
 var x = 0;
 var main_state = 1;
 
+
+// Task 3-1
 function disp() {
 	x++;
 	document.getElementById("mcount").innerHTML = x;
 
 }
 
+// Task 3-2
 function get_time() {
-	var d = new Date();
-	var z = document.getElementsByClassName("post-time");
-	z[0].innerHTML = d;
 
+
+
+	var d = document.createElement("div");
+	d.innerHTML = new Date();
+	d.className = "post-time";
+	var z = document.getElementById("posts");
+	z.appendChild(d);
+
+
+	/*  OLD CODE
+	var date = document.createElement("div");
+	var node = document.createTextNode("This is new.")
+	date.appendChild = node;
+	var d = new Date();
+	date.innerHTML = d;
+	date.setAttributes("class") =  "post-time";
+	document.getElementById("posts").appendChild(date);
+	*/
+
+
+	var text_disp = document.createElement("div");
 	var text = document.getElementById("post_text").value;
-	document.getElementsByClassName("post-content")[0].innerHTML = text;
+	text_disp.innerHTML = text;
+	text_disp.className = "post-content";
+	z.appendChild(text_disp);
 
  
 }
+// Task 3-3
 function hide_main() {
 	if (main_state == 1) {
 		document.getElementById("main").style.display = "none";
